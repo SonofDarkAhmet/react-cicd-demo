435e5f6d943b12328eb24537f3c21613da277244
Sonofdarkahmet <sonofdarkahmet@gmail.com>
2026-04-09 15:04:19 +0200

    test for skip ci [skip ci]

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css'
+import "./App.css";
 // this is app component
+// Just to test skip ci option in github actions
 function App() {
-
   return (
     <>
       <h1>CI/CD using Github Actions</h1>
       <h2>For beginners v2</h2>
       <h2>With 2 projects</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
